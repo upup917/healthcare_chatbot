8044367de17a872b7e772529ec8f3938525c0ddc
upup917 <psuwit.upup.dwrp@gmail.com>
2026-05-15 14:16:57 +0700

    edit runtime/app

--- rasa-line-bot/app.js
+++ rasa-line-bot/app.js
@@ -11,7 +11,8 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const TOKEN    = process.env.LINE_ACCESS_TOKEN;
-const RASA_URL = process.env.RASA_URL || "http://localhost:5005/webhooks/rest/webhook";
+const RASA_URL = process.env.RASA_URL
+  || (process.env.RASA_HOSTPORT ? `http://${process.env.RASA_HOSTPORT}/webhooks/rest/webhook` : "http://localhost:5005/webhooks/rest/webhook");
 
 const LINE_REPLY_URL = "https://api.line.me/v2/bot/message/reply";
 const LINE_PUSH_URL  = "https://api.line.me/v2/bot/message/push";
